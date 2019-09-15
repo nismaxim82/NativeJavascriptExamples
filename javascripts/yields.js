64363140fbc2bd5a41276c38f90519d6316c843f
Maxim Nisenblat <P0024891@ness-tech.co.il>
2019-09-15 12:03:47 +0300

    add code colorize, yields add additional example

--- javascripts/yields.js
+++ javascripts/yields.js
@@ -21,7 +21,36 @@ function* getTestJson() {
     return responseBody;
 };
 
-execute(getTestJson(), null, function(data) {
+function* registerUser({ login, password }) {
+    console.log(login, password);
+    // for our test example it will return all our users, in the real api it will be server side check user exist in the database or other data source
+    var url = 'https://nismaxim82.github.io/NativeJavascriptExamples/data/checkUserExists.json';
+    var response = yield fetch(url);
+    var users = yield response.json();
+    // check is user with login MyTestLogin already exists
+    if (users.some(function (user) {
+        return user.Login === login;
+    })) {
+        console.error('User \'' + login + '\' already exists. Please change your login.');
+        login += '2';
+        execute(registerUser(), login, password);
+    } else {
+        // call register api, we call json result with static data for our test
+        url = 'https://nismaxim82.github.io/NativeJavascriptExamples/data/registerUser.json';
+        response = yield fetch(url);
+        var registerResult = yield response.json();
+        if (!registerResult.Success) {
+            console.error(registerResult.ErrorMessage);
+        } else {
+            console.log('User successfully registered');
+        }
+    }
+}
+
+execute(getTestJson(), null, function (data) {
     console.log(data);
 });
-console.log('Asynchronious console log before received request result');
+console.log('Asynchronious console log before receive request result');
+
+// example of call linked apis
+execute(registerUser({ login: 'MyTestLogin', password: '123' }));
